14eec00a9ea7e355824c361c6477738618fefb8a
David-Nicholas <cocoreanudavid@yahoo.com>
2024-12-14 13:46:18 +0200

    Add theme feature and country detection

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
+import { ThemeProvider } from "../context/ThemeContext";
+import { LocationProvider } from "../context/LocationContext"; // Import the LocationProvider
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider>
+      <LocationProvider> {/* Wrap the stack with the LocationProvider */}
+        <Stack screenOptions={{ headerShown: false }} />
+      </LocationProvider>
+    </ThemeProvider>
+  );
 }
---
+++ app/settings.tsx
@@ -0,0 +1,59 @@
+import { View, StyleSheet } from "react-native";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+import Colors from "../constants/Colors";
+import CardSwitch from "../components/CardSwitch";
+import { AntDesign } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function Settings() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const themeColors = Colors[theme];
+  const router = useRouter();
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: themeColors.background, paddingTop: insets.top },
+      ]}
+    >
+      {/* Back Icon */}
+      <TouchableOpacity
+        style={[styles.iconContainer, { top: insets.top + 16 }]}
+        onPress={() => router.push("/")}
+      >
+        <AntDesign name="closecircleo" size={24} color={themeColors.text} />
+      </TouchableOpacity>
+
+      {/* Content Section */}
+      <View style={styles.content}>
+        <CardSwitch
+          title="Theme"
+          description="Modify the theme to light or dark mode."
+          value={theme === "dark"}
+          onValueChange={toggleTheme}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  iconContainer: {
+    position: "absolute",
+    right: 16,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "flex-start",
+    marginTop: 60, // Adjust content to avoid overlap with the icon
+  },
+});
